bd1efd6eab0d53502567a997d9c3d89bbdc3c1cb
Dmytro Prytula <prituladima@gmail.com>
2020-03-15 20:51:28 +0200

    CommentDetail.js

---
+++ src/CommentDetail.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import faker from "faker";
+// CommentDetail
+/**
+ * picsrc
+ * author
+ * date
+ * comment
+ */
+const CommentDetail = props => {
+    // const date = new Date().toLocaleString();
+    return (
+        <div className="comment">
+
+            <a href="/" className="avatar">
+                <img alt="avatar" src={props.picsrc}/>
+            </a>
+
+            <div className="content">
+                <a href="/" className="author">{props.author}</a>
+                <div className="metadata">
+                    <span className="date">{props.date}</span>
+                </div>
+                <div className="text">{props.comment}</div>
+            </div>
+        </div>
+    )
+};
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail.js';
 
 const App = () => {
+    // var date = new Date().toLocaleString();
+    // var state = {
+    //     date: new Date().toLocaleString()
+    // };
+    // const date = new Date().toLocaleString();
+    const simpleComment = 'Simple comment';
     return (
-        <div>Hi there!</div>
+        <div className="ui container comments">
+            <CommentDetail picsrc={faker.image.avatar()}
+                           author={faker.name.firstName(0)}
+                           date={new Date().toLocaleString()}
+                           comment={simpleComment}
+            />
+            <CommentDetail picsrc={faker.image.avatar()}
+                           author={faker.name.firstName(0)}
+                           date={new Date().toLocaleString()}
+                           comment={simpleComment}
+            />
+            <CommentDetail picsrc={faker.image.avatar()}
+                           author={faker.name.firstName(0)}
+                           date={new Date().toLocaleString()}
+                           comment={simpleComment}
+            />
+            <CommentDetail picsrc={faker.image.avatar()}
+                           author={faker.name.firstName(0)}
+                           date={new Date().toLocaleString()}
+                           comment={simpleComment}
+            />
+        </div>
     );
 };
 
-ReactDOM.render(<App  />, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'));
 
 
 // https://semantic-ui.com/
@@ -15,3 +44,5 @@ ReactDOM.render(<App  />, document.querySelector('#root'));
 // https://cdnjs.com/libraries/semantic-ui
 
 // https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css
+
+//npm install --save faker
